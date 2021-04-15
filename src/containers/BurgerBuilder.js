ff65df62b87027fcd0f4f44ae5a20f49d1da45c1
Alireza Ebadighajari <alirezaebg@gmail.com>
2021-04-14 23:49:40 -0700

    Use array reduce method for an empty burger

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -4,10 +4,10 @@ import Burger from "../components/Burger";
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
+            salad: 0,
+            bacon: 0,
             cheese: 2,
-            meat: 2,
+            meat: 0,
         }
     }
 
